b2233cf080a18975d14e43bc01d5e213be6ca49c
bhtanvin0 <bhtanvin1836@gmail.com>
2022-11-25 12:40:16 +0600

    mongo setup

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oaehupb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vg6bj1z.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 async function run() {
     try {
